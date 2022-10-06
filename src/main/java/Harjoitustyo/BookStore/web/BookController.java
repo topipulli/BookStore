ad357be23b1e5aca4f83d89a172548d16199e455
topipulli <topi.pulli@myy.haaga-helia.fi>
2022-10-06 20:11:02 +0300

    REST API added

--- src/main/java/Harjoitustyo/BookStore/web/BookController.java
+++ src/main/java/Harjoitustyo/BookStore/web/BookController.java
@@ -1,5 +1,8 @@
 package Harjoitustyo.BookStore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -28,6 +31,16 @@ public class BookController {
 			model.addAttribute("books", repository.findAll());
 			return "booklist";
 		}
+		
+	    @RequestMapping(value="/books", method = RequestMethod.GET)
+	    public @ResponseBody List<Book> bookListRest() {	
+	        return (List<Book>) repository.findAll();
+	    }    
+
+	    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+	    public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long id) {	
+	    	return repository.findById(id);
+	    } 
 		
 		@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
 		public String deleteBook(@PathVariable("id") Long bookId, Model model){ 
